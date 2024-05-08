aa2be874a91a96a7894a07a74502fb1c95795194
Tudor Ifrim <ifrimtudor@protonmail.com>
2024-05-08 20:14:25 +0300

    Added a new module to solution 'WFFramework' for UI related add-ons for Windows Forms. Added to it a proof-of-concept and testing ground for navigation. Will later be removed when final design is decided as to use as DLL.

---
+++ WFFramework/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFFramework
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2;
+        Form3 form3;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            form2 = new Form2();
+            form2.TopLevel = false;
+            form2.FormBorderStyle = FormBorderStyle.None;
+            this.panel1.Controls.Add(form2);
+
+            form3 = new Form3();
+            form3.TopLevel = false;
+            form3.FormBorderStyle = FormBorderStyle.None;
+            this.panel1.Controls.Add(form3);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form3.Hide();
+            form2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            form2.Hide();
+            form3.Show();
+        }
+    }
+}
