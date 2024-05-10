0cd3b8f4c8a518730a1f10bf0fd62f21afc575dc
Tudor Ifrim <ifrimtudor@protonmail.com>
2024-05-10 09:51:40 +0300

    Add file headers for license and description to the files.

--- WFFramework/FormNavigationStack.cs
+++ WFFramework/FormNavigationStack.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/************************************************************************************
+*                                                                                   *
+*  File:        FormNavigationStack                                                 *
+*  Copyright:   (c) 2024, Ifrim Tudor                                               *
+*  E-mail:      tudor-nicolae.ifrim@student.tuiasi.ro                               *
+*  Description: Provides a container for navigation between Forms based on stacks.  *
+*                                                                                   *
+*                                                                                   *
+*  This code and information is provided "as is" without warranty of                *
+*  any kind, either expressed or implied, including but not limited                 *
+*  to the implied warranties of merchantability or fitness for a                    *
+*  particular purpose. You are free to use this source code in your                 *
+*  applications as long as the original copyright notice is included.               *
+*                                                                                   *
+************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
