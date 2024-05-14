2bb2820c5d5440d7cfb9f2561ec53dfe3db62123
Andrei-Cristina <andrei-marian.cristina@student.tuiasi.ro>
2024-05-14 23:08:20 +0300

    Added some detail to both game info boxes

--- Helpers/DetailedGameInfoBox.Designer.cs
+++ Helpers/DetailedGameInfoBox.Designer.cs
@@ -33,12 +33,18 @@
             this.gameGenre = new System.Windows.Forms.Label();
             this.gamePlaytime = new System.Windows.Forms.Label();
             this.gameRating = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.gamePictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gamePictureBox
             // 
-            this.gamePictureBox.Location = new System.Drawing.Point(24, 20);
+            this.gamePictureBox.Location = new System.Drawing.Point(14, 9);
             this.gamePictureBox.Name = "gamePictureBox";
             this.gamePictureBox.Size = new System.Drawing.Size(127, 88);
             this.gamePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -49,8 +55,9 @@
             // 
             this.gameName.AutoEllipsis = true;
             this.gameName.AutoSize = true;
+            this.gameName.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.gameName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameName.Location = new System.Drawing.Point(197, 20);
+            this.gameName.Location = new System.Drawing.Point(228, 17);
             this.gameName.Name = "gameName";
             this.gameName.Size = new System.Drawing.Size(51, 20);
             this.gameName.TabIndex = 1;
@@ -60,8 +67,9 @@
             // 
             this.gameGenre.AutoEllipsis = true;
             this.gameGenre.AutoSize = true;
+            this.gameGenre.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.gameGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameGenre.Location = new System.Drawing.Point(197, 68);
+            this.gameGenre.Location = new System.Drawing.Point(228, 65);
             this.gameGenre.Name = "gameGenre";
             this.gameGenre.Size = new System.Drawing.Size(51, 20);
             this.gameGenre.TabIndex = 2;
@@ -71,8 +79,9 @@
             // 
             this.gamePlaytime.AutoEllipsis = true;
             this.gamePlaytime.AutoSize = true;
+            this.gamePlaytime.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.gamePlaytime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gamePlaytime.Location = new System.Drawing.Point(527, 68);
+            this.gamePlaytime.Location = new System.Drawing.Point(541, 65);
             this.gamePlaytime.Name = "gamePlaytime";
             this.gamePlaytime.Size = new System.Drawing.Size(51, 20);
             this.gamePlaytime.TabIndex = 3;
@@ -82,27 +91,78 @@
             // 
             this.gameRating.AutoEllipsis = true;
             this.gameRating.AutoSize = true;
+            this.gameRating.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.gameRating.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameRating.Location = new System.Drawing.Point(527, 20);
+            this.gameRating.Location = new System.Drawing.Point(541, 17);
             this.gameRating.Name = "gameRating";
             this.gameRating.Size = new System.Drawing.Size(51, 20);
             this.gameRating.TabIndex = 4;
             this.gameRating.Text = "label4";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.gamePictureBox);
+            this.panel1.Controls.Add(this.gamePlaytime);
+            this.panel1.Controls.Add(this.gameRating);
+            this.panel1.Controls.Add(this.gameName);
+            this.panel1.Controls.Add(this.gameGenre);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(687, 110);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Genre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rating:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(453, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Playtime (min.):";
+            // 
             // DetailedGameInfoBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.gameRating);
-            this.Controls.Add(this.gamePlaytime);
-            this.Controls.Add(this.gameGenre);
-            this.Controls.Add(this.gameName);
-            this.Controls.Add(this.gamePictureBox);
+            this.Controls.Add(this.panel1);
             this.Name = "DetailedGameInfoBox";
-            this.Size = new System.Drawing.Size(883, 132);
+            this.Size = new System.Drawing.Size(695, 119);
             ((System.ComponentModel.ISupportInitialize)(this.gamePictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -113,5 +173,10 @@
         private System.Windows.Forms.Label gameGenre;
         private System.Windows.Forms.Label gamePlaytime;
         private System.Windows.Forms.Label gameRating;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
