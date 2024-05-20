e6951c3e81e155ae67b05968da7174f8bb6ee4da
gccAlexandruDarie <101355884+gccAlexandruDarie@users.noreply.github.com>
2024-05-20 20:30:13 +0300

    Added missing summaries

--- UserDB_Manager/SessionInterface.cs
+++ UserDB_Manager/SessionInterface.cs
@@ -1,4 +1,4 @@
-﻿/**************************************************************************
+/**************************************************************************
  *                                                                        *
  *  File:        DatabaseManager                                          *
  *  Copyright:   (c) 2024, Darie Alexandru                                *
@@ -23,6 +23,7 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using static LibraryCommons.LibraryCommons;
 
 namespace UserDB_Manager
 {
@@ -31,11 +32,34 @@ namespace UserDB_Manager
     /// </summary>
     interface SessionInterface
     {
-        //
+        /// <summary>
+        /// This method is used to add a new game to the database
+        /// </summary>
+        /// <param name="game"> The game to be added </param>
         void AddGame(ref Game game);                    
-        void RemoveGame(ref Game game);                    
+
+        /// <summary>
+        /// This method is used to remove a game from the database
+        /// </summary>
+        /// <param name="game">Game to be removed</param>
+        void RemoveGame(ref Game game);                  
+        /// <summary>
+        /// This method is used to update a game from the database
+        /// </summary>
+        /// <param name="initialGame"> The game to be updated </param>
+        /// <param name="updatedGame"> The updated game </param>
         void UpdateGame(ref Game initialGame, ref Game updatedGame);
+
+        /// <summary>
+        /// This method is used to get all the games from the database
+        /// </summary>
+        /// <returns> A list of all the games </returns>
         List<Game> GetAllGames();
+        /// <summary>
+        /// This method is used to get a game from the database
+        /// </summary>
+        /// <param name="game"> The game to be retrieved </param>
+        /// <returns> The game </returns>
         Game GetGame(ref Game game);
     }
 }
