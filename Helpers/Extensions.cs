b8f4d6ff08e5144e8a458b8d8e633fdf087fa2d4
Tudor Ifrim <ifrimtudor@protonmail.com>
2024-05-27 18:27:12 +0300

    Finished my part of the code with headers and extra documentation.

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -1,6 +1,6 @@
 ﻿/************************************************************************************
 *                                                                                   *
-*  File:        Exetensions                                                         *
+*  File:        Extensions                                                         *
 *  Copyright:   (c) 2024, Ifrim Tudor                                               *
 *  E-mail:      tudor-nicolae.ifrim@student.tuiasi.ro                               *
 *  Description: C# extensions of all kinds.                                         *
@@ -29,11 +29,11 @@ namespace ExtensionMethods
     public static class Extensions
     {
         /// <summary>
-        /// Extends the Forms class to make a form behave properly when
+        /// Extends the Forms class to make a form suitable for placing
         /// inside of a container. A containerable form is one which
         /// isn't top level and doesn't have any borders.
         /// </summary>
-        /// <param name="f"></param>
+        /// <param name="f">The form to make containerable.</param>
         public static void MakeContainerable(this Form f)
         {
             if (f != null)
@@ -43,8 +43,19 @@ namespace ExtensionMethods
             }
         }
 
+        /// <summary>
+        /// Defines a generic handler for click. Since it's generic, it
+        /// doesn't need any parameters and it doesn't return anything.
+        /// </summary>
         public delegate void ClickHandler();
 
+        /// <summary>
+        /// Adds a ClickHandler to every control inside of the given control.
+        /// As such, a click will be registered everywhere in the frame of the
+        /// control regardless of the hierarchy.
+        /// </summary>
+        /// <param name="c">The control to add the handler to.</param>
+        /// <param name="clickHandler">The handler to execute when clicking.</param>
         public static void AddGlobalClick(this Control c, ClickHandler clickHandler)
         {
             foreach (Control control in c.Controls)
